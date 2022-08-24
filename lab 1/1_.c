61fc35eebe12aca0bbeb94a76f58f5a15075f8c4
Subodh Shrestha <shresthasubodh73@gmail.com>
2022-08-24 11:05:25 +0545

    cg

---
+++ lab 1/1_.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+#include<dos.h>
+int main(){
+    int gd = DETECT, gm;
+    initgraph(&gd,&gm,"c://turbo3//bgi")
+    line(50,50,200,200)   
+}
