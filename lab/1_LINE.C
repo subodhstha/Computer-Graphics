9831557cacaa34ebb9afe9ef4cd97966de72b0f8
Subodh Shrestha <shresthasubodh73@gmail.com>
2022-08-30 21:38:44 +0545

    lab

--- lab/1_LINE.C
+++ lab/1_LINE.C
@@ -6,11 +6,11 @@ void main()
 {
     int x1, y1, x2, y2;
     int gd = DETECT, gm;
-    initgraph(&gd, &gm,"c:\\turboc3\\bgi");
     printf("Enter the the initial coordinate\n");
     scanf("%d %d",&x1, &y1);
     printf("Enter the the final coordinate\n");
     scanf("%d %d",&x2, &y2);
+    initgraph(&gd, &gm,"c:\\turboc3\\bgi");
     printf("Subodh");
     line(x1, y1, x2, y2);
     outtextxy(x1+5,y1-5, "(x1,y1)");
